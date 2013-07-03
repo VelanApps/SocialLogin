c8584773943d202c2b4e65b734bb9c65472cab9a
root <root@ubuntu.(none)>
2013-07-03 19:27:19 +0530

    final modification of gem

--- lib/generators/social_login/social_login_generator.rb
+++ lib/generators/social_login/social_login_generator.rb
@@ -43,7 +43,7 @@ class SocialLoginGenerator < Rails::Generators::Base
                 "config.devise_model_name = '"+model_name.to_s+"'"
               end  #EO application while 
          end #EO File exists user.rb if
-        puts "\n================================================================================\n"
+         puts "\n=====================================================================================\n"
     else
          if yes?("Would you like to install Devise?")
             gem("devise")
@@ -54,7 +54,7 @@ class SocialLoginGenerator < Rails::Generators::Base
              application do
                 "config.devise_model_name = '"+model_name.to_s+"'"
               end #EO application while
-           puts "\n===============================================================================\n"   
+           puts "\n===================================================================================\n"   
          end #EO Devise install if 
     end #EO File exists devise.rb if
   end #EO def install_devise
@@ -63,94 +63,91 @@ class SocialLoginGenerator < Rails::Generators::Base
   def add_values_application_controller
       inject_into_class "app/controllers/application_controller.rb", "ApplicationController" do
           " helper_method :current_user \n " +
+          " after_filter :path \n"+
           "  private \n" +
           "  def current_user \n" +
           "    table_name = Rails.application.config.devise_model_name.titlecase \n"+
           "    @table_name = table_name.constantize \n"+
           "    @current_user ||= @table_name.find(session[:user_id]) if session[:user_id] \n\n" +
           "  end \n\n" +
+          "  def path \n"+
+          "    path= request.path \n"+
+          "    notice = request.flash[:notice] \n"+
+          "    @devise_model_name = Rails.application.config.devise_model_name \n"+
+          "    if path == '/'+@devise_model_name+'s/sign_up' && notice == 'This is one time process' \n"+
+          "      count =+ 1 \n"+
+          "       if count == 1 \n"+
+          "         session[:last_get_url] = '1' \n"+
+          "       end \n"+
+          "    else \n"+
+          "      count = 0 \n"+
+          "      session[:last_get_url] = '0' \n"+
+          "    end \n"+
+          "  end \n\n"+
           "  def after_sign_in_path_for(current_user) \n"+
-          "    table_name = Rails.application.config.devise_model_name.titlecase \n"+
-          "    @table_name = table_name.constantize\n"+
-          "    @user = @table_name.find_by_id(current_user) \n " +
-          "    @socialAccounts = SocialAccount.find_by_user_id(@user.id.to_s) \n\n"+
-          "    if @user \n"+
-          "      if @socialAccounts \n "+
-          "         session[:user_id] = @user.id \n"+
-          "         sign_in(@user) \n"+
-          "         return root_path \n"+
-          "      else \n"+
-          "         @newaccount = SocialAccount.new \n"+
-          "         @newaccount.user_id = @user.id \n"+
-          "         @newaccount.provider = session[:auth_provider] \n"+
-          "         @newaccount.uid = session[:auth_uid] \n"+
-          "         @newaccount.account_name = session[:auth_account_name] \n"+
-          "         @newaccount.save! \n"+
-          "         session[:user_id] = @user.id \n"+
-          "         sign_in(@user) \n"+
-          "         return root_path \n"+
-          "      end \n"+
-          "   else \n"+
-          "      session[:auth_provider] =nil \n"+
-          "      session[:auth_uid] =nil \n"+
-          "      session[:auth_account_name]=nil \n"+
-          "      session[:user_id] = nil \n"+
-          "      flash[:notice] = 'Not yet registered' \n"+
-          "      return root_path \n"+
-          "   end \n"+
-          "   end \n\n"
+          "    last_get_url = session[:last_get_url] \n"+
+          "     if session[:last_get_url].to_s == '1' \n"+
+          "        @devise_model_name = Rails.application.config.devise_model_name \n"+
+          "        table_name = Rails.application.config.devise_model_name.titlecase \n"+
+          "        @table_name = table_name.constantize\n"+
+          "        @user = @table_name.find_by_id(current_user) \n " +
+          "        @socialAccounts = SocialAccount.find_by_user_id(@user.id.to_s) \n\n"+
+          "         if @socialAccounts \n "+
+          "            session[:user_id] = @user.id \n"+
+          "            sign_in(@user) \n"+
+          "            return root_path \n"+
+          "         else \n"+
+          "            @newaccount = SocialAccount.new \n"+
+          "            @newaccount.user_id = @user.id \n"+
+          "            @newaccount.provider = session[:auth_provider] \n"+
+          "            @newaccount.uid = session[:auth_uid] \n"+
+          "            @newaccount.account_name = session[:auth_account_name] \n"+
+          "            @newaccount.save! \n"+
+          "            session[:user_id] = @user.id \n"+
+          "            sign_in(@user) \n"+
+          "            return root_path \n"+
+          "         end \n"+
+          "    else \n"+
+          "       session.delete(:auth_provider) \n"+
+          "       session.delete(:auth_uid) \n"+
+          "       session.delete(:auth_account_name) \n"+
+          "       return root_path \n"+
+          "    end \n"+
+          "  end \n\n"
       end #EO inject_into_class
  end #EO add_values_application_controller
-  
-  #defining function to add values in routes.rb file 
-  def add_devise_routes
-    puts "\n==============================================================================================\n"
-    puts " Kindly place the routes in routes.rb file\n"
-    puts " match '/auth/:provider/callback', :to => 'sessions#create' \n"
-    puts " match '/signout', :to => 'sessions#destroy' , :as => :signout \n"
-    puts "\n==============================================================================================\n"
-  end #EO def add_devise_routes
   
   #defining function to migrate the migration file for social accounts
   def create_migration_file
     sleep 1
     migration_template 'migration.rb', 'db/migrate/create_social_accounts.rb'
     copy_file "social_account.rb", "app/models/social_account.rb"
-  end #EO def create_migration_file
-  
-  #defining function to create omniatuh intializer file
-  def create_ominiauth_intializer_file
-   copy_file "omniauth.rb", "config/initializers/omniauth.rb"
-  end #EO def create_ominiauth_intializer_file
-  
-  #defining function to create sessions controller file
-  def create_controller_file
+    copy_file "omniauth.rb", "config/initializers/omniauth.rb"
     copy_file "sessions_controller.rb", "app/controllers/sessions_controller.rb"
-  end #EO def create_controller_file
-  
-  #defining function to create sessions view file
-  def create_view_file
-    puts "\n===========================================================================\n"
-    puts " Kindly place the code in the html file you need to create the social login \n"
-    puts "<%= render 'sessions/social_login' %>"
-    puts "\n============================================================================\n"
     copy_file "_social_login.html.erb", "app/views/sessions/_social_login.html.erb"
-  end #EO def create_view_file
-  
-  #defining function to create images file
-  def create_images_file
     copy_file "twitter_64.png", "app/assets/images/twitter_64.png"
     copy_file "facebook_64.png", "app/assets/images/facebook_64.png"
     copy_file "linkedin_64.png", "app/assets/images/linkedin_64.png"
-  end #EO def create_images_file
-  
-  #defining function to create yml file
-  def create_yml_file
-    puts "\n======================================================================================\n"
-    puts " Kindly Replace the Consumer Key and Consumer Secret of your App in the .yml files"
-    puts "\n======================================================================================\n"
     copy_file "twitter.yml", "config/twitter.yml"
     copy_file "facebook.yml", "config/facebook.yml"
     copy_file "linkedin.yml", "config/linkedin.yml"
-  end #EO def create_yml_file
+  end #EO def create_migration_file
+  
+  #defining function to create comments
+  def create_comments
+    puts "\n==========================================================================================\n"
+    puts "   1) Kindly place the routes in routes.rb file: \n\n"
+    puts "        match '/auth/:provider/callback', :to => 'sessions#create' \n"
+    puts "        match '/signout', :to => 'sessions#destroy' , :as => :signout \n\n"
+    puts "   2) Kindly place the code in the html file you need to create the social login: \n\n"
+    puts "       <%= render 'sessions/social_login' %> \n\n"
+    # puts "   3) Kindly paste the style in application.css in your 'app/assets/stylesheets': \n\n"+
+    # puts "        .notice \n "+
+    # puts "          { \n"+
+    # puts "             padding: 5px; \n "+
+    # puts "             font-weight:bold; \n"+
+    # puts "          }\n\n"+
+    puts "   3) Kindly Replace the Consumer Key and Consumer Secret of your App in the .yml files"
+    puts "\n==========================================================================================\n"
+  end #EO def create_comments
 end #EO class for Social Login Generator
--- lib/generators/social_login/templates/sessions_controller.rb
+++ lib/generators/social_login/templates/sessions_controller.rb
@@ -1,80 +1,81 @@
 class SessionsController < ApplicationController
     
-        def index
-          
-        end
-        
-        def create
-              auth = request.env["omniauth.auth"]
-              @socialAccounts = SocialAccount.find_by_provider_and_uid(auth["provider"], auth["uid"])
-              @devise_model_name = Rails.application.config.devise_model_name
-              table_name = Rails.application.config.devise_model_name.titlecase
-              @table_name = table_name.constantize
-              @user = @table_name.find(session[:user_id]) if session[:user_id]
-              if @user
-                 @userAlready = SocialAccount.find_by_user_id(@user.id.to_s)
-                 if @userAlready         
-                     submission_hash = {
-                                        "provider" => auth["provider"],
-                                        "uid" => auth["uid"],
-                                        "account_name" => auth["info"]["name"]}
-                                            
-                     @userAlready.update_attributes(submission_hash)
-                     flash[:notice] = "Already Signed in!"
-                     session[:user_id] = @user.id
-                     redirect_to "/"
-                else
-                    if  @socialAccounts 
-                        submission_hash = {
-                                          "provider" => auth["provider"],
-                                          "uid" => auth["uid"],
-                                          "account_name" => auth["info"]["name"]}
-                        @socialAccounts.update_attributes(submission_hash)
-                        flash[:notice] = "Already Signed in!"
-                        session[:user_id] = @socialAccounts.user_id
-                        @user = @table_name.find(session[:user_id]) if session[:user_id]
-                        sign_in(@user)
-                        redirect_to "/"
-                    else               
-                       @newaccount = SocialAccount.new
-                       @newaccount.user_id = @user.id
-                       @newaccount.provider = session[:auth_provider]
-                       @newaccount.uid =  session[:auth_uid]
-                       @newaccount.account_name = session[:auth_account_name]
-                       @newaccount.save!
-                       flash[:notice] = "Signed in successfully!"
-                       session[:user_id] = @user.id
-                       redirect_to "/"
-                    end
-                end 
-             else
-                if  @socialAccounts
-                       submission_hash = {
-                                          "provider" => auth["provider"],
-                                          "uid" => auth["uid"],
-                                          "account_name" => auth["info"]["name"]}
-                        @socialAccounts.update_attributes(submission_hash)
-                       flash[:notice] = "Already Signed in!"
-                       session[:user_id] = @socialAccounts.user_id
-                       @user = @table_name.find(session[:user_id]) if session[:user_id]
-                       sign_in(@user)
-                       redirect_to "/"
-               else
-                   session[:auth_provider] = auth["provider"]
-                   session[:auth_uid] = auth["uid"]
-                   session[:auth_account_name] = auth["info"]["name"]
-                   flash[:notice] = "\n This is one time process \n"
-                   redirect_to "/"+@devise_model_name+"s/sign_up"
-               end
-             end
-        end
+  def index
     
-        def destroy
-          @devise_model_name = Rails.application.config.devise_model_name
-          table_name = Rails.application.config.devise_model_name.titlecase
-          @table_name = table_name.constantize
-          session[:user_id] = nil
-          signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(@devise_model_name))
-          redirect_to "/", :notice => "Signed out!"
-        end
+  end
+  
+  def create
+        auth = request.env["omniauth.auth"]
+        @socialAccounts = SocialAccount.find_by_provider_and_uid(auth["provider"], auth["uid"])
+        @devise_model_name = Rails.application.config.devise_model_name
+        table_name = Rails.application.config.devise_model_name.titlecase
+        @table_name = table_name.constantize
+        @user = @table_name.find(session[:user_id]) if session[:user_id]
+        if @user
+           @userAlready = SocialAccount.find_by_user_id(@user.id.to_s)
+           if @userAlready         
+               submission_hash = {
+                                  "provider" => auth["provider"],
+                                  "uid" => auth["uid"],
+                                  "account_name" => auth["info"]["name"]}
+                                      
+               @userAlready.update_attributes(submission_hash)
+               flash[:notice] = "Already Signed in!"
+               session[:user_id] = @user.id
+               redirect_to "/"
+          else
+              if  @socialAccounts 
+                  submission_hash = {
+                                    "provider" => auth["provider"],
+                                    "uid" => auth["uid"],
+                                    "account_name" => auth["info"]["name"]}
+                  @socialAccounts.update_attributes(submission_hash)
+                  flash[:notice] = "Already Signed in!"
+                  session[:user_id] = @socialAccounts.user_id
+                  @user = @table_name.find(session[:user_id]) if session[:user_id]
+                  sign_in(@user)
+                  redirect_to "/"
+              else               
+                 @newaccount = SocialAccount.new
+                 @newaccount.user_id = @user.id
+                 @newaccount.provider = session[:auth_provider]
+                 @newaccount.uid =  session[:auth_uid]
+                 @newaccount.account_name = session[:auth_account_name]
+                 @newaccount.save!
+                 flash[:notice] = "Signed in successfully!"
+                 session[:user_id] = @user.id
+                 redirect_to "/"
+              end
+          end 
+       else
+          if  @socialAccounts
+                 submission_hash = {
+                                    "provider" => auth["provider"],
+                                    "uid" => auth["uid"],
+                                    "account_name" => auth["info"]["name"]}
+                  @socialAccounts.update_attributes(submission_hash)
+                 flash[:notice] = "Already Signed in!"
+                 session[:user_id] = @socialAccounts.user_id
+                 @user = @table_name.find(session[:user_id]) if session[:user_id]
+                 sign_in(@user)
+                 redirect_to "/"
+         else
+             session[:auth_provider] = auth["provider"]
+             session[:auth_uid] = auth["uid"]
+             session[:auth_account_name] = auth["info"]["name"]
+             flash[:notice] = "This is one time process"
+             redirect_to "/"+@devise_model_name+"s/sign_up"
+         end
+      end
+ end
+
+ def destroy
+   @devise_model_name = Rails.application.config.devise_model_name
+   session.delete(:auth_provider)
+   session.delete(:auth_uid)
+   session.delete(:auth_account_name)
+   session.delete(:user_id)
+   signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(@devise_model_name))
+   redirect_to "/", :notice => "Signed out!"
+ end
 end
